1fbe7268f8e555559b767c8eb03441111cf231d5
madforre <33324825+madforre@users.noreply.github.com>
2018-05-16 18:18:12 +0900

    d
    d

--- web/app/Post.php
+++ web/app/Post.php
@@ -13,4 +13,6 @@ class Post extends Model
     // create() 메소드로 모델 인스턴스를 생성할 때는 해당 모델에 $fillable 속성을 지정해 주어야 한다. 
     // 폼을 통해 사용자가 넘긴 값을 그대로 DB에 넣을 경우를 대비해, 악의적인 필드가 입력되는 것을 방지하기 위한 조치이다. 
     // Post와 Author 모델을 열고 $fillable 속성을 지정하자.
+    
+    // 
 }
